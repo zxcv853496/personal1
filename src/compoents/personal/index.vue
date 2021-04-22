611182e6dd5b8992de9d03e5fa2b89a5bb63f820
zxcv853496 <zxcv853496@gmail.com>
2021-04-22 18:48:19 +0800

    personal

--- src/compoents/personal/index.vue
+++ src/compoents/personal/index.vue
@@ -11,19 +11,28 @@ export default {
                     name:'陳峻暐',
                     englishName:'chen-jun-wei',
                     img:'/img/personal-1.jpg',
-                    content:'專題:AI人臉辨識-自動簽到打卡系統',
+                    content:[
+                        '大手攜小手-物聯網程式設計競賽',
+                        '專題:AI人臉辨識-自動簽到打卡系統',
+                    ]
                 },
                 {
                     name:'許維元',
                     englishName:'chen-jun-wei',
                     img:'/img/personal-2.jpg',
-                    content:'專題:AI人臉辨識-自動簽到打卡系統',
+                    content:[
+                        '大手攜小手-物聯網程式設計競賽',
+                        '專題:AI人臉辨識-自動簽到打卡系統',
+                    ]
                 },
                 {
                     name:'徐偉成',
                     englishName:'chen-jun-wei',
                     img:'/img/personal-3.jpg',
-                    content:'專題:AI人臉辨識-自動簽到打卡系統',
+                    content:[
+                        '大手攜小手-物聯網程式設計競賽',
+                        '專題:AI人臉辨識-自動簽到打卡系統',
+                    ]
                 },
             ]
         }
